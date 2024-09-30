4a1d38cc02edf4b04b514a10fd6d57e18a96e499
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-09-30 11:42:52 +0530

    Update linked-list-cycle-2.py

--- linked-list-cycle-2.py
+++ linked-list-cycle-2.py
@@ -1,49 +1,50 @@
 # LinkedList Cycle II
+# https://www.codingninjas.com/codestudio/problems/1112628
+# https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+from utils import ListNode
 
 
-# https://www.codingninjas.com/codestudio/problems/1112628?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
-class Node:
-    def __init__(self, data):
-
-        self.data = data
-        self.next = None
-        self.pos = []
-
-    def __del__(self):
-        if self.next:
-            del self.next
+class SolutionBrute:
+    def detectCycle(self, head):
+        memo = set()
+        while head:
+            if head in memo:
+                return head
+            memo.add(head)
+            head = head.next
 
 
-def firstNode(head):
-    memo = {}
-
-    pos = 0
-    node = head
-    while node is not None:
-        if node in memo:
-            return memo[node]
-        memo[node] = pos
-        pos += 1
-        node = node.next
-
-    return -1
-
-
-first = Node(1)
-
-sec = Node(2)
-first.next = sec
-
-thir = Node(3)
-sec.next = thir
-
-four = Node(4)
-thir.next = four
-
-five = Node(5)
-four.next = five
-
-# five.next = four
+class SolutionBetter:
+    def detectCycle(self, head):
+        d = ListNode(next=head)
+        slow = d.next
+        fast = d.next
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        slow = d.next
+        while slow and fast and fast.next:
+            if slow == fast:
+                return slow
+            slow = slow.next
+            fast = fast.next
 
 
-print(firstNode(first))
+head1 = ListNode.as_linked_list([3, 2, 0, -4], pos=1)
+head2 = ListNode.as_linked_list([1, 2], pos=0)
+head3 = ListNode.as_linked_list([1], pos=-1)
+head4 = ListNode.as_linked_list(
+    [
+        7032, 15013, 6890, 8877, 11344, 320, 13037, 9414, 6817, 1566,
+        14907, -2756, 9931, -4488, 11602, 4887, 1239, 6231
+    ],
+    pos=-1,
+)
+
+print(SolutionBetter().detectCycle(head1))
+print(SolutionBetter().detectCycle(head2))
+print(SolutionBetter().detectCycle(head3))
+print(SolutionBetter().detectCycle(head4))
