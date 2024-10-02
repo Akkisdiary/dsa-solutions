a3af0b43b63f4bcf51b5566fb05b0ef19a007f8d
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-10-02 21:38:27 +0530

    Update test_runner.py

--- utils/test_runner.py
+++ utils/test_runner.py
@@ -15,6 +15,10 @@ class BColors:
     UNDERLINE = "\033[4m"
 
 
+def _yellow(s):
+    return f"{BColors.WARNING}{s}{BColors.ENDC}"
+
+
 def _green(s):
     return f"{BColors.OKGREEN}{s}{BColors.ENDC}"
 
@@ -24,29 +28,32 @@ def _red(s):
 
 
 class TestRunner:
-    def __init__(self, fn) -> None:
+    def __init__(self, fn, tc) -> None:
         self.run = fn
-        self.input = None
-        self.expected = None
+        self.test_cases = tc
 
-    def case(self, case):
-        self.input = case["input"]
-        self.expected = case["expected"]
+    def _test(self, case, serialize=(lambda x: x)):
+        input = case["input"]
+        expected = case["expected"]
+
+        try:
+            s = time.time()
+            output = self.run(**deepcopy(input))
+            e = time.time()
+
+            if serialize(output) == serialize(expected):
+                print(f"{_green('PASSED')} input: {input}, rt: {e - s}")
+            else:
+                print(f"{_red('FAILED')} input: {input}")
+                print(f"{'-'*6} expected: {_red(expected)}")
+                print(f"{'-'*6} output: {_red(output)}")
+        except Exception:
+            print(f"Runtime Error, input: {input}")
+            print(_red(traceback.format_exc()))
+
         return self
 
     def test(self, serialize=(lambda x: x)):
-        s = time.time()
-        try:
-            output = self.run(**deepcopy(self.input))
-        except Exception:
-            print(f"Runtime Error, input: {self.input}")
-            print(_red(traceback.format_exc()))
-            return
-        e = time.time()
-        if serialize(output) == serialize(self.expected):
-            print(f"{_green('PASSED')} input: {self.input}, rt: {e - s}")
-        else:
-            print(f"{_red('FAILED')} input: {self.input}")
-            print(f"{'-'*6} expected: {_red(self.expected)}")
-            print(f"{'-'*6} output: {_red(output)}")
-        return self
+        for i in range(len(self.test_cases)):
+            print(_yellow(f"Running test {i}..."))
+            self._test(self.test_cases[i], serialize)
