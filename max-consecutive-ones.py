e7ad2a3f41fe7d0e6f990d0d6b29587379540e8b
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-10-03 12:00:50 +0530

    Create max-consecutive-ones.py

---
+++ max-consecutive-ones.py
@@ -0,0 +1,66 @@
+# 485. Max Consecutive Ones
+# https://leetcode.com/problems/max-consecutive-ones/description/
+
+from utils import TestRunner
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums) -> int:
+        ans = 0
+        s = 0
+        for i in nums:
+            if i == 1:
+                s += 1
+            else:
+                s = 0
+            ans = max(ans, s)
+        return ans
+
+
+test_cases = [
+    {
+        "input": {
+            "nums": [1, 1, 0, 1, 1, 1],
+        },
+        "expected": 3,
+    },
+    {
+        "input": {
+            "nums": [1, 0, 1, 1, 0, 1],
+        },
+        "expected": 2,
+    },
+    {
+        "input": {
+            "nums": [1, 1, 1, 1],
+        },
+        "expected": 4,
+    },
+    {
+        "input": {
+            "nums": [0, 0, 0, 0],
+        },
+        "expected": 0,
+    },
+    {
+        "input": {
+            "nums": [0, 0, 1],
+        },
+        "expected": 1,
+    },
+    {
+        "input": {
+            "nums": [1, 0, 0],
+        },
+        "expected": 1,
+    },
+    {
+        "input": {
+            "nums": [],
+        },
+        "expected": 0,
+    },
+]
+
+if __name__ == "__main__":
+    TestRunner(SolutionBrute().findMaxConsecutiveOnes, test_cases).test()
