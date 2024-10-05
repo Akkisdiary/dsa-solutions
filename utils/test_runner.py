b6fbac995e632c765a2482399ebcd4ea02d66567
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-10-05 16:10:46 +0530

    Update test_runner.py

--- utils/test_runner.py
+++ utils/test_runner.py
@@ -27,12 +27,19 @@ def _red(s):
     return f"{BColors.FAIL}{s}{BColors.ENDC}"
 
 
+def _to_str(s, limit=30):
+    s = str(s)
+    if len(s) > limit:
+        return f"{s[:limit]}..."
+    return s
+
+
 class TestRunner:
     def __init__(self, fn, tc) -> None:
         self.run = fn
         self.test_cases = tc
 
-    def _test(self, case, serialize=(lambda x: x)):
+    def _test(self, test_no, case, serialize=(lambda x: x)):
         input = case["input"]
         expected = case["expected"]
 
@@ -41,19 +48,21 @@ class TestRunner:
             output = self.run(**deepcopy(input))
             e = time.time()
 
+            input = _to_str(input)
+            rt = round((e - s) * 1000, 6)
             if serialize(output) == serialize(expected):
-                print(f"{_green('PASSED')} input: {input}, rt: {e - s}")
+                print(f"{test_no}. {_green('PASSED')} input={input}, {rt=}ms")
             else:
-                print(f"{_red('FAILED')} input: {input}")
-                print(f"{'-'*6} expected: {_red(expected)}")
-                print(f"{'-'*6} output: {_red(output)}")
+                pad = "." * (8 + len(str(test_no)))
+                print(f"{test_no}. {_red('FAILED')} input={input}")
+                print(f"{pad} expected: {_red(expected)}")
+                print(f"{pad} output: {_red(output)}")
         except Exception:
-            print(f"Runtime Error, input: {input}")
+            print(f"Runtime Error, input={input}")
             print(_red(traceback.format_exc()))
 
         return self
 
     def test(self, serialize=(lambda x: x)):
         for i in range(len(self.test_cases)):
-            print(_yellow(f"Running test {i}..."))
-            self._test(self.test_cases[i], serialize)
+            self._test(i, self.test_cases[i], serialize)
