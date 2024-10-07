a1bb0f81cccc7c7a5939631a87a7083f8cbe713b
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-10-07 12:43:12 +0530

    Update subset-sum.py

--- subset-sum.py
+++ subset-sum.py
@@ -1,27 +1,40 @@
 # Subset Sum
+# Subset Sum
+# https://www.geeksforgeeks.org/problems/subset-sums2234/1
+# https://www.codingninjas.com/codestudio/problems/subset-sum_3843086
 
-# https://www.codingninjas.com/codestudio/problems/subset-sum_3843086?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
-
-
-def subsetSum(nums):
-    n = len(nums)
-
-    subsetSums = [0]
-
-    def findSubsets(i, prevSum):
-        if i >= n:
-            return
-
-        findSubsets(i + 1, prevSum)
-        # findSubsets(i+1, nums[i])
-        findSubsets(i + 1, prevSum + nums[i])
-
-        subsetSums.append(prevSum + nums[i])
-
-    findSubsets(0, 0)
-
-    subsetSums.sort()
-    return subsetSums
+from utils import TestRunner
 
 
-print(subsetSum([1, 2, 3]))
+class SolutionBrute:
+    def subsetSums(self, arr, n):
+        return self.solve(arr, n, 0, [], 0)
+
+    def solve(self, arr, n, i, ans, rsum):
+        if i == n:
+            ans.append(rsum)
+            return ans
+        self.solve(arr, n, i+1, ans, rsum)
+        self.solve(arr, n, i+1, ans, rsum+arr[i])
+        return ans
+
+
+test_cases = [
+    {
+        "input": {
+            "arr": [2, 3],
+            "n": 2,
+        },
+        "expected": [0, 2, 3, 5],
+    },
+    {
+        "input": {
+            "arr": [1, 2, 1],
+            "n": 3,
+        },
+        "expected": [0, 1, 1, 2, 2, 3, 3, 4],
+    },
+]
+
+if __name__ == "__main__":
+    TestRunner(SolutionBrute().subsetSums, test_cases).test(sorted)
