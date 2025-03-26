fa5281a5f3a85bf95e7e892736d2b576a34695c3
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2025-03-26 18:27:48 +0530

    create sort-a-stack

---
+++ sort-a-stack.py
@@ -0,0 +1,21 @@
+"""
+Sort a Stack
+https://www.naukri.com/code360/problems/sort-a-stack_985275
+"""
+
+from utils import TestRunner
+
+
+class SolutionBrute:
+    def sortStack(self, stack):
+        return list(stack).sort()
+
+
+cases = [
+    {"input": {"stack": [4, 1, 2]}, "expected": [4, 2, 1]},
+    {"input": {"stack": [5, -2, 9, -7, 3]}, "expected": [9, 5, 3, -2, -7]},
+    {"input": {"stack": [-3, 14, 18, -5, 30]}, "expected": [30, 18, 14, -3, -5]},
+]
+
+if __name__ == "__main__":
+    TestRunner(SolutionBrute().sortStack).test(cases)
