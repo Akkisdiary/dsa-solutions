a2687eb5d92a9d73d96c4d69be46af18b35f7af2
Akkisdiary <Akkisdiary@users.noreply.github.com>
2026-04-07 15:47:01 +0000

    Auto format using ruff

--- valid-palindrome-ii.py
+++ valid-palindrome-ii.py
@@ -11,7 +11,9 @@ class Solution:
         l, r = 0, len(s) - 1
         while l <= r:
             if s[l] != s[r]:
-                return self.isPalindrome(s, l + 1, r) or self.isPalindrome(s, l, r - 1)
+                return self.isPalindrome(s, l + 1, r) or self.isPalindrome(
+                    s, l, r - 1
+                )
             l += 1
             r -= 1
         return True
